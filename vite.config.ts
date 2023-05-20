b629ace23de088abe2180ef3f201f8dbe9b722ea
truc0 <22969604+truc0@users.noreply.github.com>
2023-05-20 15:17:30 +0800

    feat(pwa): add PWA support via vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
